ccaa00aa7ba68b622143742c362b47cdeb1daa3a
theCephas <oshoiseoluwa@gmail.com>
2022-06-21 00:35:08 -0700

    first commit

---
+++ about.js
@@ -0,0 +1,7 @@
+let want = confirm("Please fill up the short survey about you.")
+let yourName = prompt("Enter your full name(Surname first)");
+let yourHeight = prompt("Enter your height (ex: 5'7feet): ");
+let yourCountry = prompt("What is the name of your country?");
+
+
+    alert("My name is " + yourName + "." + "\nI am " + yourHeight + " tall." + "\nI am from " + yourCountry + ".");
